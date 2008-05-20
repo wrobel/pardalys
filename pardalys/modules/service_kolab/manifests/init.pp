523fb1038447d7d37f4db94ff9c8ca3f652cd8a5
Gunnar Wrobel <p@rdus.de>
2008-05-20 13:58:40 +0200

    Added new module that provides the basic kolab files and should later hold the bootstrapping process.

---
+++ pardalys/modules/service_kolab/manifests/init.pp
@@ -0,0 +1,35 @@
+# Class service::kolab
+#
+#  Provides basic Kolab server elements
+#
+# @author Gunnar Wrobel <p@rdus.de>
+# @version 1.0
+# @package service_kolab
+#
+# @fact operatingsystem Allows to choose the correct package name
+#                       depending on the operating system
+# @fact keyword         The keyword for the system which is used to
+#                       select unstable packages
+#
+class service::kolab {
+
+  $fqdnhostname    = get_var('kolab_fqdnhostname',    get_var('kolab_bootstrap_fqdnhostname', $hostname))
+  $is_master       = get_var('kolab_is_master',       get_var('kolab_bootstrap_is_master',    'true'))
+  $domain          = get_var('kolab_domain',          get_var('kolab_bootstrap_domain',       $fqdnhostname)
+  $base_dn         = get_var('kolab_base_dn',         get_var('kolab_bootstrap_base_dn',      dnfromdomain($domain))
+  $bind_pw         = get_var('kolab_bind_pw',         get_var('kolab_bootstrap_bind_pw',      generate("$bindir/openssl rand -base64 12"))
+  $bind_pw_sq      = shellquote($pw)
+  $bind_pw_hash    = generate("$sbindir$/slappasswd -s $bind_pw_sq")
+  $ldap_uri        = get_var('kolab_ldap_uri',        'ldap://127.0.0.1:389')
+  # We do not support slave setup at the moment
+  $ldap_master_uri = get_var('kolab_ldap_master_uri', 'ldap://127.0.0.1:389')
+  $php_pw          = get_var('kolab_php_pw',          generate("$bindir/openssl rand -base64 30"))
+  $calendar_pw     = get_var('kolab_calendar_pw',     generate("$bindir/openssl rand -base64 30"))
+
+  file { 
+    "$kolabconfigfile":
+    content => template('service_kolab/kolab.conf')
+    "$kolabglobalsfile":
+    content => template('service_kolab/kolab.globals')
+  }
+}
---
+++ pardalys/modules/service_kolab/plugins/facter/kolab.rb
@@ -0,0 +1,49 @@
+# Get the Kolab settings for our system
+
+if FileTest.file?(Facter.kolabconfigfile)
+  File.open(Facter.kolabconfigfile).each do |line|
+    var = $1 and value = $2 if line =~ /^(.+):(.+)$/
+    if var != nil && value != nil
+      facts[var.trim()] = value.trim()
+      var = nil
+      value = nil
+    end
+  end
+  facts.each{|var,val|
+    Facter.add('kolab_' + var) do
+      setcode do
+        val
+      end
+    end
+  }
+end
+
+if FileTest.file?(Facter.kolabbootstrapfile)
+  File.open(Facter.kolabbootstrapfile).each do |line|
+    var = $1 and value = $2 if line =~ /^(.+):(.+)$/
+    if var != nil && value != nil
+      facts[var.trim()] = value.trim()
+      var = nil
+      value = nil
+    end
+  end
+  facts.each{|var,val|
+    Facter.add('kolab_bootstrap_' + var) do
+      setcode do
+        val
+      end
+    end
+  }
+  Facter.add('kolab_bootstrap') do
+    setcode do
+      true
+    end
+  end
+else
+  Facter.add('kolab_bootstrap') do
+    setcode do
+      false
+    end
+  end
+end
+

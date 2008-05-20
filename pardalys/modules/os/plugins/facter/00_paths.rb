53dfc558d29ec8c519e09859d73a86278bf2767a
Gunnar Wrobel <p@rdus.de>
2008-05-20 15:39:38 +0200

    Fixed typos.

--- pardalys/modules/os/plugins/facter/00_paths.rb
+++ pardalys/modules/os/plugins/facter/00_paths.rb
@@ -1,15 +1,19 @@
 # Set basic system paths
 
 if Facter.operatingsystem == 'Gentoo'
-  Facter.add('sysconfdir')    do setcode do '/etc'                        end
-  Facter.add('localstatedir') do setcode do '/var'                        end
-  Facter.add('sysrundir')     do setcode do Facter.localstatedir + '/run' end
-  Facter.add('statelibdir')   do setcode do Facter.localstatedir + '/lib' end
-  Facter.add('logdir')        do setcode do Facter.localstatedir + '/log' end
+  Facter.add('sysconfdir')    do setcode do '/etc'                        end end
+  Facter.add('sbindir')       do setcode do '/usr/sbin'                   end end
+  Facter.add('bindir')        do setcode do '/usr/bin'                    end end
+  Facter.add('localstatedir') do setcode do '/var'                        end end
+  Facter.add('sysrundir')     do setcode do Facter.localstatedir + '/run' end end
+  Facter.add('statelibdir')   do setcode do Facter.localstatedir + '/lib' end end
+  Facter.add('logdir')        do setcode do Facter.localstatedir + '/log' end end
 else
-  Facter.add('sysconfdir')    do setcode do '/etc'                        end
-  Facter.add('localstatedir') do setcode do '/var'                        end
-  Facter.add('sysrundir')     do setcode do Facter.localstatedir + '/run' end
-  Facter.add('statelibdir')   do setcode do Facter.localstatedir + '/lib' end
-  Facter.add('logdir')        do setcode do Facter.localstatedir + '/log' end
+  Facter.add('sysconfdir')    do setcode do '/etc'                        end end
+  Facter.add('sbindir')       do setcode do '/usr/sbin'                   end end
+  Facter.add('bindir')        do setcode do '/usr/bin'                    end end
+  Facter.add('localstatedir') do setcode do '/var'                        end end
+  Facter.add('sysrundir')     do setcode do Facter.localstatedir + '/run' end end
+  Facter.add('statelibdir')   do setcode do Facter.localstatedir + '/lib' end end
+  Facter.add('logdir')        do setcode do Facter.localstatedir + '/log' end end
 end

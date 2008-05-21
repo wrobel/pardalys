210d01a1caf15eadfc8ad4e6f57afc9313b420a6
Gunnar Wrobel <p@rdus.de>
2008-05-21 14:13:46 +0200

    Fix setting the openldap facts.

--- pardalys/modules/service_openldap/plugins/facter/01_openldap_paths.rb
+++ pardalys/modules/service_openldap/plugins/facter/01_openldap_paths.rb
@@ -1,21 +1,21 @@
 # Set basic system paths
 
 if Facter.operatingsystem == 'Gentoo'
-  Facter.add('openldap_confdir')   do setcode do Facter.sysconfdir + '/openldap'       end
-  Facter.add('openldap_schemadir') do setcode do Facter.openldap_confdir + '/schema'   end
-  Facter.add('openldap_datadir')   do setcode do Facter.statelibdir + '/openldap-data' end
-  Facter.add('openldap_pidfile')   do setcode do Facter.sysrundir + '/slapd.pid' end
-  Facter.add('openldap_argsfile')  do setcode do Facter.sysrundir + '/slapd.args' end
-  Facter.add('openldap_usr')       do setcode do 'root' end
-  Facter.add('openldap_rusr')      do setcode do 'ldap' end
-  Facter.add('openldap_grp')       do setcode do 'ldap' end
+  Facter.add('openldap_confdir')   do setcode do Facter.sysconfdir + '/openldap'       end end
+  Facter.add('openldap_schemadir') do setcode do Facter.openldap_confdir + '/schema'   end end
+  Facter.add('openldap_datadir')   do setcode do Facter.statelibdir + '/openldap-data' end end
+  Facter.add('openldap_pidfile')   do setcode do Facter.sysrundir + '/slapd.pid' end end
+  Facter.add('openldap_argsfile')  do setcode do Facter.sysrundir + '/slapd.args' end end
+  Facter.add('openldap_usr')       do setcode do 'root' end end
+  Facter.add('openldap_rusr')      do setcode do 'ldap' end end
+  Facter.add('openldap_grp')       do setcode do 'ldap' end end
 else
-  Facter.add('openldap_confdir')   do setcode do Facter.sysconfdir + '/openldap'       end
-  Facter.add('openldap_schemadir') do setcode do Facter.openldap_confdir + '/schema'   end
-  Facter.add('openldap_datadir')   do setcode do Facter.statelibdir + '/openldap-data' end
-  Facter.add('openldap_pidfile')   do setcode do Facter.sysrundir + '/slapd.pid' end
-  Facter.add('openldap_argsfile')  do setcode do Facter.sysrundir + '/slapd.args' end
-  Facter.add('openldap_usr')       do setcode do 'root' end
-  Facter.add('openldap_rusr')      do setcode do 'ldap' end
-  Facter.add('openldap_grp')       do setcode do 'ldap' end
+  Facter.add('openldap_confdir')   do setcode do Facter.sysconfdir + '/openldap'       end end
+  Facter.add('openldap_schemadir') do setcode do Facter.openldap_confdir + '/schema'   end end
+  Facter.add('openldap_datadir')   do setcode do Facter.statelibdir + '/openldap-data' end end
+  Facter.add('openldap_pidfile')   do setcode do Facter.sysrundir + '/slapd.pid' end end
+  Facter.add('openldap_argsfile')  do setcode do Facter.sysrundir + '/slapd.args' end end
+  Facter.add('openldap_usr')       do setcode do 'root' end end
+  Facter.add('openldap_rusr')      do setcode do 'ldap' end end
+  Facter.add('openldap_grp')       do setcode do 'ldap' end end
 end

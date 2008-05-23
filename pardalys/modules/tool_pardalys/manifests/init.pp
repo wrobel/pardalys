f289383b69800f30d3e97d922b993ccf587494a0
Gunnar Wrobel <p@rdus.de>
2008-05-23 10:10:28 +0200

    Added moulde tool_pardalys.

---
+++ pardalys/modules/tool_pardalys/manifests/init.pp
@@ -0,0 +1,100 @@
+import 'os_gentoo'
+
+# Class tool::pardalys
+#
+#  Provides the package installation for p@rdalys
+#
+# @author Gunnar Wrobel <p@rdus.de>
+# @version 1.0
+# @package tool_pardalys
+#
+# @fact operatingsystem Allows to choose the correct package name
+#                       depending on the operating system
+# @fact keyword         The keyword for the system which is used to
+#                       select unstable packages
+#
+class tool::pardalys {
+
+  # Package installation
+  case $operatingsystem {
+    gentoo:
+    {
+      gentoo_use_flags { rake:
+        context => 'tool_pardalys',
+        package => 'dev-ruby/rake',
+        use     => 'doc',
+        tag     => 'buildhost'
+      }
+      package { rake:
+        category => 'dev-ruby',
+        ensure   => 'installed',
+        require  =>  Gentoo_use_flags['rake'],
+        tag      => 'buildhost'
+      }
+      gentoo_use_flags { rspec:
+        context => 'tool_pardalys',
+        package => 'dev-ruby/rspec',
+        use     => 'doc',
+        tag     => 'buildhost'
+      }
+      package { rspec:
+        category => 'dev-ruby',
+        ensure   => 'installed',
+        require  =>  Gentoo_use_flags['rspec'],
+        tag      => 'buildhost'
+      }
+      gentoo_use_flags { mocha:
+        context => 'tool_pardalys',
+        package => 'dev-ruby/mocha',
+        use     => 'doc',
+        tag     => 'buildhost'
+      }
+      gentoo_keywords { mocha:
+        context  => 'tool_pardalys',
+        package  => '=dev-ruby/mocha-0.5.6',
+        keywords => "~$keyword",
+        tag      => 'buildhost'
+      }
+      package { mocha:
+        category => 'dev-ruby',
+        ensure   => 'installed',
+        require  =>  [ Gentoo_keywords['mocha'],
+                       Gentoo_use_flags['mocha'] ],
+        tag      => 'buildhost'
+      }
+      gentoo_use_flags { git:
+        context => 'tool_pardalys',
+        package => 'dev-utils/git',
+        use     => 'emacs bash-completion',
+        tag     => 'buildhost'
+      }
+      package { git:
+        category => 'dev-utils',
+        ensure   => 'installed',
+        require  =>  Gentoo_use_flags['git'],
+        tag      => 'buildhost'
+      }
+      # As the tool will be in development for a while we don't add a
+      # version restriction for now
+      gentoo_keywords { pardalys:
+        context  => 'tool_pardalys',
+        package  => 'app-admin/pardalys',
+        keywords => "~$keyword",
+        tag      => 'buildhost'
+      }
+      package { pardalys:
+        category => 'app-admin',
+        ensure   => 'installed',
+        require  =>  [ Gentoo_keywords['pardalys'],
+                       Gentoo_use_flags['pardalys'] ],
+        tag      => 'buildhost'
+      }
+    }
+    default:
+    {
+      package { openldap:
+        ensure   => 'latest',
+      }
+    }
+  }
+}

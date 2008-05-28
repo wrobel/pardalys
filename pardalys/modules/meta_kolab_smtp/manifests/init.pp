af7a7846753d2148a116d68bd08bfb174e0787f5
Gunnar Wrobel <p@rdus.de>
2008-05-28 09:58:50 +0200

    Include the sasl module.

--- pardalys/modules/meta_kolab_smtp/manifests/init.pp
+++ pardalys/modules/meta_kolab_smtp/manifests/init.pp
@@ -1,6 +1,7 @@
 import 'os'
 import 'service_kolab'
 import 'service_openldap'
+import 'service_sasl'
 import 'service_postfix'
 
 # Class meta::kolab::smtp
@@ -24,6 +25,7 @@ class meta::kolab::smtp {
   include service::kolab
   include service::openldap
   include service::openldap::serve
+  include service::sasl
   include service::postfix
 
 }

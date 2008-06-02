598e8b0dd149b3fe71c8d3b92519276a62850dff
Gunnar Wrobel <p@rdus.de>
2008-06-02 10:54:18 +0200

    Update to the newer structures.

--- pardalys/modules/service_clamav/manifests/init.pp
+++ pardalys/modules/service_clamav/manifests/init.pp
@@ -1,121 +1,29 @@
 import 'os_gentoo'
 
 # Class service::clamav
+#
 #  Provides a configuration for the clamav virus scanner
 #
-# Required parameters 
+# @author Gunnar Wrobel <p@rdus.de>
+# @version 1.0
+# @package service_postfix
 #
-#  * service_clamav_amavis: Is clamav running as subprocess to amavis?
+# @module root           The root module is required to determine the installed
+#                        amavis version.
+# @module os             The os module is required to determine basic system
+#                        paths.
+# @module os_gentoo      The os_gentoo module is required for Gentoo specific
+#                        package installation.
+# @module service_kolab  Provides the kolab configuration.
 #
-#  * service_clamav_syslog: Should clamav report to syslog?
-#
-# Optional parameters 
-#
-#  You can override additional internal variable with external global
-#  variables.
-#
-# Templates
-#
-#  * templates/clamd.conf_*: Configuration for the clamd service.
-#
-#  * templates/freshclam.conf_*: Configuration for the freshclam service.
+# @fact operatingsystem  Allows to choose the correct package name
+#                        depending on the operating system. In addition
+#                        required to set additional tasks depending on
+#                        the distribution.
+# @fact version_clamav   The clamav version currently installed
+# @fact clamav_syslog    Should clamav log to syslog?
 #
 class service::clamav {
-
-  $ruser = $operatingsystem ? {
-    gentoo  => $service_clamav_amavis ? {
-      yes => 'amavis',
-      no  => 'clamav'
-    },
-    default => 'clamav'
-  }
-
-  if $service_clamav_clamd_conf {
-    $clamd_conf = $service_clamav_clamd_conf
-  } else {
-    $clamd_conf = $operatingsystem ? {
-      gentoo  => '/etc/clamd.conf',
-      default => '/etc/clamd.conf'
-    }
-  }
-  if $service_clamav_freshclam_conf {
-    $clamd_conf = $service_clamav_freshclam_conf
-  } else {
-    $freshclam_conf = $operatingsystem ? {
-      gentoo  => '/etc/freshclam.conf',
-      default => '/etc/freshclam.conf'
-    }
-  }
-
-  if $service_clamav_logdir {
-    $logdir = $service_clamav_logdir
-  } else {
-    $logdir =  $operatingsystem ? {
-      gentoo  => '/var/log/clamav',
-      default => '/var/log/clamav'
-    }
-  }
-
-  if $service_clamav_clamd_logfile {
-    $clamd_logfile = $service_clamav_clamd_logfile
-  } else {
-    $clamd_logfile = $operatingsystem ? {
-      gentoo  => '${logdir}/clamd.log',
-      default => '${logdir}/clamd.log'
-    }
-  }
-
-  if $service_clamav_freshclam_logfile {
-    $freshclam_logfile = $service_clamav_freshclam_logfile
-  } else {
-    $freshclam_logfile = $operatingsystem ? {
-      gentoo  => '${logdir}/freshclam.log',
-      default => '${logdir}/freshclam.log'
-    }
-  }
-
-  if $service_clamav_rundir {
-    $rundir = $service_clamav_rundir
-  } else {
-    $rundir =  $operatingsystem ? {
-      gentoo  => '/var/run/clamav',
-      default => '/var/run/clamav'
-    }
-  }
-  $clamd_pidfile = $operatingsystem ? {
-    gentoo  => '${rundir}/clamd.pid',
-    default => '${rundir}/clamd.pid'
-  }
-  $freshclam_pidfile = $operatingsystem ? {
-    gentoo  => '${rundir}/freshclam.pid',
-    default => '${rundir}/freshclam.pid'
-  }
-
-  if $service_clamav_socket {
-    $socket = $service_clamav_socket
-  } else {
-    $socket = $operatingsystem ? {
-      gentoo  => $service_clamav_amavis ? {
-        no  =>'${rundir}/clamd.sock',
-	yes =>'/var/amavis/clamd.sock'
-      },
-      default => '${rundir}/clamd.sock'
-    }
-  }
-
-  if $service_clamav_datadir {
-    $datadir = $service_clamav_datadir
-  } else {
-    $datadir = $operatingsystem ? {
-      gentoo  => $service_clamav_amavis ? {
-        no  =>'/var/lib/clamav',
-	yes =>'/var/amavis/clamav'
-      },
-      default => '/var/lib/clamav'
-    }
-  }
-
-  $syslog = $service_clamav_syslog
 
   # Package installation
   case $operatingsystem {
@@ -127,8 +35,7 @@ class service::clamav {
         use     => 'mailwrapper nls',
         tag     => 'buildhost'
       }
-      package { clamav:
-        name     => 'clamav',
+      package { 'clamav':
         category => 'app-antivirus',
         ensure   => 'installed',
         require  => Gentoo_use_flags['clamav'],
@@ -137,85 +44,87 @@ class service::clamav {
     }
     default:
     {
-      package { clamav:
-        name     => 'clamav',
-        ensure   => 'installed',
+      package { 'clamav':
+        ensure   => 'installed';
       }
     }
+  }
+
+  # Initialize template variables
+  $template_clamav = template_version($version_clamav, '0.93@:0.93,', '0.93')
+
+  $clamd_conf        = "${os::sysconfdir}/clamd.conf"
+  $freshclam_conf    = "${os::sysconfdir}/freshclam.conf"
+  $logdir            = "${os::logdir}/clamav"
+  $clamd_logfile     = "${logdir}/clamd.log"
+  $freshclam_logfile = "${logdir}/freshclam.log"
+  $rundir            = "${os::sysrundir}/clamav"
+  $clamd_pidfile     = "${rundir}/clamd.pid"
+  $freshclam_pidfile = "${rundir}/freshclam.pid"
+
+  $syslog            = get_var('clamav_syslog', true)
+  $amavis_child      = get_var('amavis_child', true)
+
+  if $amavis_child {
+    $ruser   = 'amavis'
+    $datadir = '/var/amavis/clamav'
+    $socket  = '/var/amavis/clamd.sock'
+  } else {
+    $ruser   = 'clamav'
+    $datadir = "${os::statelibdir}/clamav"
+    $socket  = "${datadir}/clamd.sock"
+  }
+
+
+  file { "$logdir":
+    ensure  => $syslog ? {
+      false   => 'directory',
+      'false' => 'directory',
+      true    => 'absent',
+      'true'  => 'absent'
+    },
+    owner   => $ruser,
+    group   => $ruser
   }
   
-  # FIXME: This should be determined automatically
-  $service_clamav_version = $operatingsystem ? {
-    gentoo  => '0.92.1',
-    default => '0.92.1'
-  }
-
-  # Rewrite the application version to the configuration version
-  # Warn if we don't know the version
-  # FIXME: This will only be useful once we know how to determine
-  # package versions
-  $clamd_template_version = $service_clamav_version ? {
-    '0.92.1' => '0.92.1'
-  }
-
-  case $syslog {
-    no: 
-    {
-      file { 'service_clamav_logdir':
-        path    => $logdir,
-        ensure  => 'directory',
-        owner   => $ruser,
-        group   => $ruser
-      }
-    }
-    yes: 
-    {
-      file { 'service_clamav_logdir':
-        path   => $logdir,
-        ensure => 'absent',
-        force  => true
-      }
-    }
-  }
-
-  file {'service_clamav_rundir':
-    path => $rundir,
+  file {
+    "$rundir":
     ensure => 'directory',
     owner   => $ruser,
-    group   => $ruser
-  }
-
-  file {'service_clamav_datadir':
-    path => $datadir,
+    group   => $ruser;
+    "$datadir":
     ensure => 'directory',
     owner   => $ruser,
-    group   => $ruser
-  }
-
-  # Configuration
-  file { 'service_clamav_clamd_conf':
-    path    => $clamd_conf,
-    content => template("service_clamav/clamd.conf_${clamd_template_version}"),
+    group   => $ruser;
+    "$clamd_conf":
+    content => template("service_clamav/clamd.conf_${template_clamav}"),
     owner   => 'root',
     group   => $ruser,
     mode    => 644,
-    require => Package['clamav']
-  }
-
-  file { 'service_clamav_freshclam_conf':
-    path    => $freshclam_conf,
-    content => template("service_clamav/freshclam.conf_${clamd_template_version}"),
+    notify  => Service['clamd'],
+    require => Package['clamav'];
+    "$freshclam_conf":
+    content => template("service_clamav/freshclam.conf_${template_clamav}"),
     owner   => 'root',
     group   => $ruser,
     mode    => 644,
-    require => Package['clamav']
+    notify  => Service['clamd'],
+    require => Package['clamav'];
   }
 
-  service { 'service_clamav_clamd' :
-    name      => 'clamd',
+  service { 'clamd':
     ensure    => 'running',
     enable    => true,
-    subscribe => File['service_clamav_clamd_conf']
+  }
+
+  case $operatingsystem {
+    gentoo: {
+      # Ensure that the service starts with the system
+      file { '/etc/runlevels/default/clamd':
+        ensure => '/etc/init.d/clamd',
+        require  => Package['clamav']
+      }
+    }
   }
 
 }

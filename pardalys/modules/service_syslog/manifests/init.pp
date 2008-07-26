0da12b9d3352852cc1e5823f52a78fcf800b5cbf
Gunnar Wrobel <p@rdus.de>
2008-07-26 22:28:41 +0200

    Corrected use of the package version.

--- pardalys/modules/service_syslog/manifests/init.pp
+++ pardalys/modules/service_syslog/manifests/init.pp
@@ -51,75 +51,58 @@ class service::syslog {
   }
   
   # Convert to template version
-  case $version_syslog_ng {
-    default: {
-      crit("Unkown syslog-ng version (Value: ${version_syslog_ng})!")
-      $template_syslog_ng = 'UNKNOWN'
-    }
-    '2.0.6' : {
-      $template_syslog_ng = '2.0.6'
+  $template_syslog_ng = template_version($version_syslog_ng, '2.0.6@2.0.9:2.0.6', '2.0.6')
+
+  $remote_ip = get_var('service_syslog_remote_ip', false)
+  $remote_port = get_var('service_syslog_remote_port', '514')
+  $accept_remote = get_var('service_syslog_accept_remote', false)
+
+  file { 
+    '/var/log/syslog.d':
+    ensure  => 'directory';
+    '/etc/syslog-ng/syslog-ng.conf':
+    content => template("service_syslog/syslog-ng.conf_${template_syslog_ng}"),
+    require => Package['syslog-ng'],
+    notify => Service['syslog-ng'];
+    '/etc/logrotate.d/syslog-ng':
+    content => template("service_syslog/logrotate.d_syslog_${template_syslog_ng}"),
+    require => Package['syslog-ng'];
+    '/etc/monit.d/syslog-ng':
+    content => template("service_syslog/monit_syslog_ng");
+    '/usr/libexec/munin/plugins/syslog_ng':
+    source  => 'puppet:///service_syslog/munin_plugin_syslog_ng',
+    mode    => 755;
+        '/etc/munin/plugins/syslog_ng':
+    ensure  => '/usr/libexec/munin/plugins/syslog_ng',
+    require => File['/usr/libexec/munin/plugins/syslog_ng'];
+    '/etc/munin/plugin-conf.d/syslog_ng':
+    source  => 'puppet:///service_syslog/munin_plugin_syslog_ng.conf';
+  }
+
+  if defined(Package['logwatch']) {
+    file { 
+      '/etc/logwatch/scripts/services/syslog-ng':
+      source  => 'puppet:///service_syslog/logwatch_plugin_syslog_ng',
+      require => Package['syslog-ng'];
+      '/etc/logwatch/conf/services/syslog-ng.conf':
+      source  => 'puppet:///service_syslog/logwatch_plugin_syslog_ng.conf',
+      require => Package['syslog-ng'];
     }
   }
 
-  case $template_syslog_ng {
-    'UNKNOWN': {
-      crit("Cannot configure package without a valid version number!")
-    }
-    default : {
+  service { 'syslog-ng':
+    ensure    => 'running',
+    enable    => true,
+    require => Package['syslog-ng']
+  }
 
-      $remote_ip = get_var('service_syslog_remote_ip', false)
-      $remote_port = get_var('service_syslog_remote_port', '514')
-      $accept_remote = get_var('service_syslog_accept_remote', false)
-
-      file { 
-        '/var/log/syslog.d':
-          ensure  => 'directory';
-        '/etc/syslog-ng/syslog-ng.conf':
-          content => template("service_syslog/syslog-ng.conf_${template_syslog_ng}"),
-          require => Package['syslog-ng'],
-          notify => Service['syslog-ng'];
-        '/etc/logrotate.d/syslog-ng':
-          content => template("service_syslog/logrotate.d_syslog_${template_syslog_ng}"),
-          require => Package['syslog-ng'];
-        '/etc/monit.d/syslog-ng':
-          content => template("service_syslog/monit_syslog_ng");
-        '/usr/libexec/munin/plugins/syslog_ng':
-          source  => 'puppet:///service_syslog/munin_plugin_syslog_ng',
-          mode    => 755;
-        '/etc/munin/plugins/syslog_ng':
-          ensure  => '/usr/libexec/munin/plugins/syslog_ng',
-          require => File['/usr/libexec/munin/plugins/syslog_ng'];
-        '/etc/munin/plugin-conf.d/syslog_ng':
-          source  => 'puppet:///service_syslog/munin_plugin_syslog_ng.conf';
+  case $operatingsystem {
+    gentoo: {
+      # Ensure that the service starts with the system
+      file { '/etc/runlevels/default/syslog-ng':
+        ensure => '/etc/init.d/syslog-ng',
+        require  => Package['syslog-ng']
       }
-
-      if defined(Package['logwatch']) {
-        file { 
-          '/etc/logwatch/scripts/services/syslog-ng':
-            source  => 'puppet:///service_syslog/logwatch_plugin_syslog_ng',
-            require => Package['syslog-ng'];
-          '/etc/logwatch/conf/services/syslog-ng.conf':
-            source  => 'puppet:///service_syslog/logwatch_plugin_syslog_ng.conf',
-            require => Package['syslog-ng'];
-        }
-      }
-
-      service { 'syslog-ng':
-        ensure    => 'running',
-        enable    => true,
-        require => Package['syslog-ng']
-      }
-
-      case $operatingsystem {
-        gentoo: {
-          # Ensure that the service starts with the system
-          file { '/etc/runlevels/default/syslog-ng':
-            ensure => '/etc/init.d/syslog-ng',
-            require  => Package['syslog-ng']
-          }
-        }
-      }
-
     }
   }
 }

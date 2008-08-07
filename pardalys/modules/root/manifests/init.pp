643bf5e03872b320635dcb15088534c47a7e26ab
Gunnar Wrobel <p@rdus.de>
2008-08-07 09:23:02 +0200

    Add the resource definition for adding lines to a file.

--- pardalys/modules/root/manifests/init.pp
+++ pardalys/modules/root/manifests/init.pp
@@ -1,5 +1,18 @@
-# Class root
-#   Provides all common functionality
-class root {
-
+# Taken from http://reductivelabs.com/trac/puppet/wiki/Recipes/SimpleText
+define line($file, $line, $ensure = 'present') {
+  case $ensure {
+    default : {
+      err ( "unknown ensure value ${ensure}" )
+    }
+    present: {
+      exec {"/bin/echo '${line}' >> '${file}'":
+        unless => "/bin/grep -qFx '${line}' '${file}'"
+      }
+    }
+    absent: {
+      exec { "/usr/bin/perl -ni -e 'print unless /^\\Q${line}\\E\$/' '${file}'":
+        onlyif => "/bin/grep -qFx '${line}' '${file}'"
+      }
+    }
+  }
 }

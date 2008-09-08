eb42604b837f856136b8c9c8565a9e4599b3b4f3
Gunnar Wrobel <p@rdus.de>
2008-09-08 17:04:56 +0200

    Add documentation to the split function.

--- pardalys/modules/root/plugins/puppet/parser/functions/split.rb
+++ pardalys/modules/root/plugins/puppet/parser/functions/split.rb
@@ -1,4 +1,13 @@
-# Return an array by splitting a variable
+# Split a string variable into an array using the specified split
+# character.
+#
+# Usage:
+#
+#   $string    = 'value1,value2'
+#   $array_var = split($string, ',')
+#
+# $array_var holds the result ['value1', 'value2']
+#
 module Puppet::Parser::Functions
   newfunction(:split, :type => :rvalue) do |args|
     return args[0].split(args[1])

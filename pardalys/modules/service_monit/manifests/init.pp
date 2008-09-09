7bddd2c519feebf148073fd30da2e93e9f7cd1da
Gunnar Wrobel <p@rdus.de>
2008-09-09 11:00:22 +0200

    Update and fix the monit configuration.

--- pardalys/modules/service_monit/manifests/init.pp
+++ pardalys/modules/service_monit/manifests/init.pp
@@ -46,48 +46,35 @@ class service::monit {
     ensure => 'directory',
   }
 
-  # Convert to template version
-  case $version_monit {
-    default: {
-      crit("Unkown monit version (Value: ${version_monit})!")
-      $template_monit = 'UNKNOWN'
-    }
-    '4.8.2' : {
-      $template_monit = '4.8.2'
+  $template_monit = template_version($version_monit, '4.10.1@:4.10.1,', '4.10.1')
+
+  $monit_user = get_var('monit_user', 'monit')
+  $monit_pass = get_var('monit_pass', 'admin')
+
+  # Monit node configuration
+  file { 'service_monit_monitrc':
+    path    => '/etc/monitrc',
+    mode    => 600,
+    content => template("service_monit/monitrc_${template_monit}"),
+    require => Package['monit'],
+    notify => Service["monit"]
+  }
+
+  service { "monit":
+    ensure => running,
+    enable => true,
+    require => Package['monit']
+  }
+
+  case $operatingsystem {
+    gentoo: {
+      # Ensure that the service starts with the system
+      file { '/etc/runlevels/default/monit':
+        ensure => '/etc/init.d/monit',
+        require  => Package['monit']
+      }
     }
   }
 
-  case $template_monit {
-    'UNKNOWN': {
-      crit("Cannot configure package without a valid version number!")
-    }
-    default : {
-      # Monit node configuration
-      file { 'service_monit_monitrc':
-        path    => '/etc/monitrc',
-        mode    => 600,
-        content => template("service_monit/monitrc_${template_monit}"),
-        require => Package['monit'],
-        notify => Service["monit"]
-      }
-
-      service { "monit":
-        ensure => running,
-        enable => true,
-        require => Package['monit']
-      }
-
-      case $operatingsystem {
-        gentoo: {
-          # Ensure that the service starts with the system
-          file { '/etc/runlevels/default/monit':
-            ensure => '/etc/init.d/monit',
-            require  => Package['monit']
-          }
-        }
-      }
-
-    }
-  }
 }
 

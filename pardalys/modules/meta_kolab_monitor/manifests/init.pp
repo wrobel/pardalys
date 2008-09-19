f3c72ca918b5a23429432d8b74d25c48e313fcc8
Gunnar Wrobel <p@rdus.de>
2008-09-19 09:22:02 +0200

    Include the SSL configuration for the monitoring host.

--- pardalys/modules/meta_kolab_monitor/manifests/init.pp
+++ pardalys/modules/meta_kolab_monitor/manifests/init.pp
@@ -1,6 +1,7 @@
 import 'os'
 import 'service_kolab'
 import 'tool_pardalys'
+import 'tool_openssl'
 import 'service_lighttpd'
 import 'service_logwatch'
 import 'service_nagios'
@@ -23,6 +24,7 @@ class meta::kolab::monitor::master {
   include os
   include service::kolab
   include tool::pardalys
+  include tool::openssl
   include service::lighttpd
   include service::logwatch
   include service::nagios

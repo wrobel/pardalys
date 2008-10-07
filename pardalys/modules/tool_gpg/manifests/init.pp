2f0ad0b662dd9f17be5baab96c475430eaf94349
Gunnar Wrobel <p@rdus.de>
2008-10-07 06:19:37 +0200

    Complete documentation.

--- pardalys/modules/tool_gpg/manifests/init.pp
+++ pardalys/modules/tool_gpg/manifests/init.pp
@@ -2,16 +2,11 @@ import 'os_gentoo'
 
 # Class tool::gpg
 #
-#  Provides the package installation for gpg
+#  Provides the package installation for gpg.
 #
 # @author Gunnar Wrobel <p@rdus.de>
 # @version 1.0
 # @package tool_gpg
-#
-# @fact operatingsystem Allows to choose the correct package name
-#                       depending on the operating system
-# @fact keyword         The keyword for the system which is used to
-#                       select unstable packages
 #
 class tool::gpg {
 

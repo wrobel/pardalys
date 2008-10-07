8a406d7bdf45a035a61e27b1d301c8f4350c0a28
Gunnar Wrobel <p@rdus.de>
2008-10-07 07:21:54 +0200

    Complete documentation and minor cleanups.

--- pardalys/modules/service_openssh/manifests/init.pp
+++ pardalys/modules/service_openssh/manifests/init.pp
@@ -1,23 +1,12 @@
 import 'os_gentoo'
 
 # Class service::openssh
+#
 #  Provides a configuration for the ssh daemon
 #
-# Required parameters 
-#
-#  *  : 
-#
-# Optional parameters 
-#
-#  *  : 
-#
-# Templates
-#
-#  * templates/ : 
-#
-# Files
-#
-#  * files/ : 
+# @author Gunnar Wrobel <p@rdus.de>
+# @version 1.0
+# @package service_openssh
 #
 class service::openssh {
 
@@ -46,52 +35,37 @@ class service::openssh {
     }
   }
   
-  # Convert to template version
-  case $version_openssh {
-    default: {
-      crit("Unkown openssh version (Value: ${version_openssh})!")
-      $template_openssh = 'UNKNOWN'
-    }
-    '4.7_p1-r6' : {
-      $template_openssh = '4.7_p1-r6'
+  $template_openssh = template_version($version_openssh, '4.7_p1-r6@:4.7_p1-r6,', '4.7_p1-r6')
+
+  $allow_root = get_var('sshd_allow_root', true)
+  $allow_pass = get_var('sshd_allow_pass', false)
+  $allow_x11  = get_var('desktop', false)
+  $sshd_run_service = get_var('run_services', true)
+
+  file { 
+    '/etc/ssh/sshd_config':
+    content => template("service_openssh/sshd_config_${template_openssh}"),
+    require => Package['openssh'];
+    '/etc/monit.d/sshd':
+    source => 'puppet:///service_openssh/monit_sshd';
+  }
+
+  if $sshd_run_service {
+    service { 'sshd':
+      ensure    => 'running',
+      enable    => true,
+      require => Package['openssh'],
+      subscribe => File['/etc/ssh/sshd_config'];
     }
   }
 
-  case $template_openssh {
-    'UNKNOWN': {
-      crit("Cannot configure package without a valid version number!")
-    }
-    default : {
-
-      $allow_root = get_var('sshd_allow_root', true)
-      $allow_pass = get_var('sshd_allow_pass', false)
-      $allow_x11  = get_var('desktop', false)
-
-      file { 
-        '/etc/ssh/sshd_config':
-          content => template("service_openssh/sshd_config_${template_openssh}"),
-          require => Package['openssh'],
-          notify => Service['sshd'];
-        '/etc/monit.d/sshd':
-          source => 'puppet:///service_openssh/monit_sshd';
-      }
-
-      service { 'sshd':
-        ensure    => 'running',
-        enable    => true,
+  case $operatingsystem {
+    gentoo: {
+      # Ensure that the service starts with the system
+      file { '/etc/runlevels/default/sshd':
+        ensure  => '/etc/init.d/sshd',
         require => Package['openssh']
       }
-
-      case $operatingsystem {
-        gentoo: {
-          # Ensure that the service starts with the system
-          file { '/etc/runlevels/default/sshd':
-            ensure => '/etc/init.d/sshd',
-            require  => Package['openssh']
-          }
-        }
-      }
-
     }
   }
 }

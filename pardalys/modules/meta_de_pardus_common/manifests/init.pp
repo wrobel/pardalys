5397611d6222797ad803c4843dbb8a4a69366d82
Gunnar Wrobel <p@rdus.de>
2008-10-07 07:25:58 +0200

    Adapt to the cleaned modules.

--- pardalys/modules/meta_de_pardus_common/manifests/init.pp
+++ pardalys/modules/meta_de_pardus_common/manifests/init.pp
@@ -1,20 +1,22 @@
 import 'os'
 import 'tool_gpg'
 import 'tool_layman'
+import 'tool_backupdir'
 import 'tool_backup'
 import 'tool_baselayout'
 import 'tool_bash'
 import 'tool_emacs'
 import 'tool_logrotate'
+import 'tool_openssl'
+import 'service_openldap'
 import 'tool_pardalys'
 import 'tool_portage'
 import 'tool_subversion'
 import 'tool_system'
-import 'client_puppet_local'
+import 'service_monit'
 import 'service_syslog'
 import 'service_cron'
 import 'service_openssh'
-import 'service_monit'
 import 'service_ssmtp'
 
 # Class meta::pardus::common
@@ -25,20 +27,21 @@ class meta::de::pardus::common {
   include os
   include tool::gpg
   include tool::layman
+  include tool::backupdir
   include tool::backup
   include tool::baselayout
   include tool::bash
   include tool::emacs
   include tool::logrotate
+  include tool::openssl
+  include service::openldap
   include tool::pardalys
   include tool::portage
   include tool::subversion
   include tool::system
-  include client::puppet::local
   include service::monit
   include service::syslog
   include service::cron
   include service::openssh
   include service::ssmtp
-
 }

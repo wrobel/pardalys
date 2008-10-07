ecfab68946f9a7b4e74c909d5880bc8efef936a4
Gunnar Wrobel <p@rdus.de>
2008-10-07 07:03:08 +0200

    Complete documentation and minor cleanups.

--- pardalys/modules/service_monit/manifests/init.pp
+++ pardalys/modules/service_monit/manifests/init.pp
@@ -2,19 +2,12 @@ import 'os'
 import 'os_gentoo'
 
 # Class service::monit
-#  Provides monit as a system monitor
 #
-# Required parameters 
+#  Provides monit as a system monitor.
 #
-#  * :
-#
-# Optional parameters 
-#
-#  * :
-#
-# Templates
-#
-#  * :
+# @author Gunnar Wrobel <p@rdus.de>
+# @version 1.0
+# @package service_monit
 #
 class service::monit {
 
@@ -49,21 +42,29 @@ class service::monit {
   $template_monit = template_version($version_monit, '4.10.1@:4.10.1,', '4.10.1')
 
   $monit_user = get_var('monit_user', 'monit')
-  $monit_pass = get_var('monit_pass', 'admin')
+  $monit_pass = get_var('monit_pass', '')
+  $monit_mailserver = get_var('mailserver', 'localhost')
+  $monit_hostname = get_var('hostname', 'localhost')
+  $monit_domainname = get_var('domainname', 'localdomain')
+  $monit_sysadmin = get_var('sysadmin', '')
+  $monit_run_service = get_var('run_services', true)
+
+  $monit_cert_path = $tool::openssl::ssl_combined_path
 
   # Monit node configuration
-  file { 'service_monit_monitrc':
-    path    => '/etc/monitrc',
+  file { '/etc/monitrc':
     mode    => 600,
     content => template("service_monit/monitrc_${template_monit}"),
     require => Package['monit'],
-    notify => Service["monit"]
   }
 
-  service { "monit":
-    ensure => running,
-    enable => true,
-    require => Package['monit']
+  if $monit_run_service {
+    service { "monit":
+      ensure => running,
+      enable => true,
+      require => Package['monit'],
+      subscribe => File['/etc/monitrc']
+    }
   }
 
   case $operatingsystem {

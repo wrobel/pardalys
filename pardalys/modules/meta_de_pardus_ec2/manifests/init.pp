b56deb69f45f79c3b233afb497b25dabede27b56
Gunnar Wrobel <p@rdus.de>
2008-10-07 13:35:27 +0200

    Started the ec2 profile.

---
+++ pardalys/modules/meta_de_pardus_ec2/manifests/init.pp
@@ -0,0 +1,15 @@
+import 'tool_ec2_kernel'
+
+# Class meta::de::pardus::ec2
+#
+#  Defines the setup for a ec2 instance.
+#
+# @author Gunnar Wrobel <p@rdus.de>
+# @version 1.0
+# @package meta_de_pardus_ec2
+#
+class meta::de::pardus::ec2 {
+
+  include tool::ec2::kernel
+
+}

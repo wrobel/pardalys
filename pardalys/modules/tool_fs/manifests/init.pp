d5e05c1352712535f73ca03939fe83c047419484
Gunnar Wrobel <p@rdus.de>
2008-10-08 14:47:01 +0000

    Add new module for net filesystem support.

---
+++ pardalys/modules/tool_fs/manifests/init.pp
@@ -0,0 +1,64 @@
+import 'root'
+import 'os_gentoo'
+
+# Class tool::fs
+#
+#  Provides tools for a variety of file systems.
+#
+# @author Gunnar Wrobel <p@rdus.de>
+# @version 1.0
+# @package tool_fs
+#
+class tool::fs {
+
+  # Package installation
+  case $operatingsystem {
+    gentoo:
+    {
+      gentoo_keywords { 's3fs':
+        context  => 'tools_fs_s3fs',
+        package  => 'sys-fs/s3fs',
+        keywords => "~$keyword",
+        tag      => 'buildhost'
+      }
+# FIXME: The fuse fs system need a decent kernel
+#       package { 's3fs':
+#         category => 'sys-fs',
+#         ensure   => 'installed',
+#         tag      => 'buildhost',
+#         require  => Gentoo_Keywords['s3fs'];
+#       }
+      package { 'fuse':
+        category => 'sys-fs',
+        ensure   => 'installed',
+        tag      => 'buildhost';
+      }
+      package { 'autofs':
+        category => 'net-fs',
+        ensure   => 'installed',
+        tag      => 'buildhost';
+      }
+
+      @line {'kernel26_fuse':
+        file => '/etc/modules.d/kernel-2.6',
+        line => 'fuse',
+        tag => 'buildhost'
+      }
+    }
+  }
+
+  $autofs_run_service = get_var('run_services', true)
+
+  if $autofs_run_service {
+    exec { "modprobe-fuse":
+      command => "/sbin/modprobe fuse",
+      unless => "/sbin/lsmod | grep fuse 1>/dev/null",
+    }
+
+    service { "autofs":
+      ensure => running,
+      enable => true,
+      require => Package['autofs']
+    }
+  }
+}

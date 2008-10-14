0d2cf5de744a496fba35d058ce8195973d9f5a2a
Gunnar Wrobel <p@rdus.de>
2008-10-14 15:54:11 +0200

    Added some desktop tools.

---
+++ pardalys/modules/tool_system_desktop/manifests/init.pp
@@ -0,0 +1,45 @@
+import 'os_gentoo'
+
+# Class tool::system::desktop
+#
+#  Provides some desktop specific tools.
+#
+# @author Gunnar Wrobel <p@rdus.de>
+# @version 1.0
+# @package tool_system_desktop
+#
+class tool::system::desktop {
+
+  # Package installation
+  case $operatingsystem {
+    gentoo:
+    {
+      # 0.6.0 does not compile for me
+      gentoo_keywords { libmcs:
+        context  => 'tool_system_desktop_libmcs',
+        package  => '=dev-libs/libmcs-0.7.1-r2',
+        keywords => "~$keyword",
+        tag      => 'buildhost'
+      }
+
+      gentoo_keywords { luma:
+        context  => 'tool_system_desktop_luma',
+        package  => '=net-nds/luma-2.4',
+        keywords => "~$keyword",
+        tag      => 'buildhost'
+      }
+      package { luma:
+        category => 'net-nds',
+        ensure   => 'installed',
+        require  =>  Gentoo_keywords['luma'],
+        tag      => 'buildhost'
+      }
+    }
+    default:
+    {
+      package { luma:
+        ensure   => 'installed',
+      }
+    }
+  }
+}

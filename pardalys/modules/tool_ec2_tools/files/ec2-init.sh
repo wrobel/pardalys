2421a224052337331ef378c13735ea4edd6d7bd7
root <root@domU-12-31-38-01-B8-13.localdomain>
2009-05-31 10:59:09 +0200

    Automatically set the hostname in /etc/hosts when booting the system.

--- pardalys/modules/tool_ec2_tools/files/ec2-init.sh
+++ pardalys/modules/tool_ec2_tools/files/ec2-init.sh
@@ -9,3 +9,6 @@ $BINDIR/ec2-import-sshkeys.sh
 
 # set permissions on the /tmp dir
 chmod 777 /tmp
+
+MYHOST=`cat /var/spool/ec2/meta-data/public-hostname`
+sed -i -e "s/^\(127.0.0.1\s*\)/\1$MYHOST /" /etc/hosts

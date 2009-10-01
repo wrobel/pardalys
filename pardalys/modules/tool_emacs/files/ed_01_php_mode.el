b20193fb129f5b0c5d9edae761a4b6493924b419
Gunnar Wrobel <p@rdus.de>
2009-10-01 20:25:04 +0200

    Add tools and cleanup

--- pardalys/modules/tool_emacs/files/ed_01_php_mode.el
+++ pardalys/modules/tool_emacs/files/ed_01_php_mode.el
@@ -7,7 +7,6 @@
 ;; http://ww1.netzologie.de/php/phpeditor-emacs.php
 ;; http://st-on-it.blogspot.com/2008/02/charged-php-mode-for-emacs-and-more.html
 
-
 ;; activate speedbar
 (setq php-mode-speedbar-open 1)
 (setq imenu-create-index-function 'imenu-default-create-index-function)
@@ -80,48 +79,6 @@
   (run-hooks 'php-mode-pear-hook)
 )
 
-;; taken from http://atomized.org/2008/10/php-lint-and-style-checking-from-emacs/
-(defun php-lint ()
-  "Performs a PHP lint check on the current file."
-  (interactive)
-  (let ((compilation-error-regexp-alist '(php))
-        (compilation-error-regexp-alist-alist ()))
-    (pushnew '(php "\\(syntax error.*\\) in \\(.*\\) on line \\([0-9]+\\)$" 2 3 nil nil 1)
-             compilation-error-regexp-alist-alist)
-    (compile (concat "php -l -f \"" (buffer-file-name) "\""))))
-
-(defun get-file-in-hierarchy (file)
-  "Searches the directory tree upwards for the given file and returns the path."
-  (setq currentdir (file-name-directory buffer-file-name))
-  (while (and (not (file-exists-p (concat currentdir file)))
-	      (not (string= currentdir "/")))
-    (setq currentdir (file-name-directory (directory-file-name currentdir))))
-  (concat currentdir file))
-
-(defun phpcs-pear ()
-  "Performs a PEAR style PHP code sniffer check on the current file."
-  (interactive)
-  (phpcs "PEAR"))
-
-(defun phpcs-zend ()
-  "Performs a Zend style PHP code sniffer check on the current file."
-  (interactive)
-  (phpcs "Zend"))
- 
-(defun phpcs (format)
-  "Performs a PHP code sniffer check on the current file."
-  (interactive)
-  (setq phpsettings (get-file-in-hierarchy ".emacs.php.el"))
-  (if (file-exists-p phpsettings)
-      (load phpsettings)
-    (setq phpcs_command "phpcs"))
-  (let ((compilation-error-regexp-alist '(gnu)))
-    (compile (format (concat phpcs_command
-			     " --standard="
-			     format
-			     " --report=emacs \"%s\"")
-                     (buffer-file-name)))))
- 
 ;; Activate PHP Horde editing
 (defun php-setenv-horde ()
   (interactive)
@@ -135,6 +92,107 @@
   (php-mode-horde-hook))
 
 (setq whitespace-modes (append '(php-mode) whitespace-modes))
+
+(defun php-compile ()
+  "Runs a compilation process with a number of PHP style checks."
+  (interactive)
+  (let ((compilation-error-regexp-alist (get-php-compilation-regexp)))
+    (setq cb (current-buffer))
+    (php-loc)
+    (switch-to-buffer "*compilation*")
+    (if (get-buffer  "*php lines of code*")
+	(kill-buffer "*php lines of code*"))
+    (rename-buffer "*php lines of code*")
+    (switch-to-buffer cb)
+    (php-pmd)
+    (switch-to-buffer "*compilation*")
+    (if (get-buffer  "*php mess detection*")
+	(kill-buffer "*php mess detection*"))
+    (rename-buffer "*php mess detection*")
+    (switch-to-buffer cb)
+    (php-cpd)
+    (switch-to-buffer "*compilation*")
+    (if (get-buffer  "*php copy paste detection*")
+	(kill-buffer "*php copy paste detection*"))
+    (rename-buffer "*php copy paste detection*")
+    (switch-to-buffer cb)
+    (phpcs)
+    (switch-to-buffer "*compilation*")
+    (if (get-buffer  "*php code style*")
+	(kill-buffer "*php code style*"))
+    (rename-buffer "*php code style*")
+    (switch-to-buffer cb)
+    (php-lint)
+    (switch-to-buffer "*compilation*")
+    (if (get-buffer  "*php lint*")
+	(kill-buffer "*php lint*"))
+    (rename-buffer "*php lint*")
+    (switch-to-buffer cb)
+    ))
+
+;; taken from http://atomized.org/2008/10/php-lint-and-style-checking-from-emacs/
+(defun php-lint ()
+  "Performs a PHP lint check on the current file."
+  (interactive)
+  (let ((compilation-error-regexp-alist (get-php-compilation-regexp)))
+    (compile (concat "php -l -f \"" (buffer-file-name) "\""))))
+
+(defun phpcs ()
+  "Performs a PHP code sniffer check on the current file."
+  (interactive)
+  (setq phpsettings (get-file-in-hierarchy ".emacs.php.el"))
+  (load phpsettings)
+  (let ((compilation-error-regexp-alist (get-php-compilation-regexp)))
+    (compile (concat phpcs_command
+		     phpcs_options
+		     " "
+                     (buffer-file-name)))))
+ 
+(defun php-loc ()
+  (interactive)
+  (if (not buffer-file-read-only)
+      (save-buffer))
+  (setq phpsettings (get-file-in-hierarchy ".emacs.php.el"))
+  (load phpsettings)
+  (let ((compilation-error-regexp-alist (get-php-compilation-regexp)))
+    (set (make-local-variable 'phploc-command)
+	 (format "%s %s %s"
+		 plc_command
+		 plc_options
+		 buffer-file-name))
+    (compile phploc-command)))
+
+(defun php-cpd ()
+  (interactive)
+  (if (not buffer-file-read-only)
+      (save-buffer))
+  (setq phpsettings (get-file-in-hierarchy ".emacs.php.el"))
+  (load phpsettings)
+  (let ((compilation-error-regexp-alist (get-php-compilation-regexp)))
+    (set (make-local-variable 'phpcpd-command)
+	 (format "%s %s %s"
+		 pcp_command
+		 pcp_options
+		 buffer-file-name))
+    (compile phpcpd-command)))
+
+(defun php-pmd ()
+  (interactive)
+  (if (not buffer-file-read-only)
+      (save-buffer))
+  (setq phpsettings (get-file-in-hierarchy ".emacs.php.el"))
+  (load phpsettings)
+  (let ((compilation-error-regexp-alist (get-php-compilation-regexp)))
+    (set (make-local-variable 'phpmd-command)
+	 (format "%s;cd %s;%s %s %s %s %s"
+		 pmd_pre
+		 (file-name-directory buffer-file-name)
+		 pmd_command
+		 buffer-file-name
+		 pmd_format
+		 pmd_codestyle
+		 pmd_options))
+    (compile phpmd-command)))
 
 (defun php-start-php-on-buffer()
   (interactive)
@@ -225,6 +283,57 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
+;; Helpers
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun get-php-compilation-regexp ()
+  (let ((php-regexp-alist ()))
+    ;; Syntax error when running the php lint process
+    (add-to-list 'php-regexp-alist
+		 '("\\(syntax error.*\\) in \\(.*\\) on line \\([0-9]+\\)$" 2 3 nil nil 1))
+
+    ;; GNU definition from compile.el used with the Code Sniffer style check
+    (add-to-list 'php-regexp-alist
+		 '("^\\(?:[[:alpha:]][-[:alnum:].]+: ?\\)?\
+\\([0-9]*[^0-9\n]\\(?:[^\n ]\\| [^-\n]\\)*?\\): ?\
+\\([0-9]+\\)\\(?:\\([.:]\\)\\([0-9]+\\)\\)?\
+\\(?:-\\([0-9]+\\)?\\(?:\\3\\([0-9]+\\)\\)?\\)?:\
+\\(?: *\\(\\(?:Future\\|Runtime\\)?[Ww]arning\\|W:\\)\\|\
+ *\\([Ii]nfo\\(?:\\>\\|rmationa?l?\\)\\|I:\\|instantiated from\\|[Nn]ote\\)\\|\
+\[0-9]?\\(?:[^0-9\n]\\|$\\)\\|[0-9][0-9][0-9]\\)"
+		   1 (2 . 5) (4 . 6) (7 . 8)))
+
+    ;; Errors from the Emacs driver of the PHP mess detector
+    (add-to-list 'php-regexp-alist
+		 '("^\\([^:]*\\):\\([0-9]+\\)[^:]+: \\(?:[Ee]rror\\|[Ww]arnin\\(g\\)\\):.+$" 1 2 nil (3) nil))
+    
+    ;; Errors when running php on a buffer
+    (add-to-list 'php-regexp-alist
+		 '("^\\(\\/.*\.php\\):\\([0-9]+\\)$" 1 2 nil nil nil))
+    (add-to-list 'php-regexp-alist
+		 '("\\(^.*[Ee]rror.*\\|^.*Exception.*\\) in \\(.*\\) on line \\([0-9]+\\)$" 2 3 nil nil 1))
+    (add-to-list 'php-regexp-alist
+		 '("[0-9]+\. \\([^ ]*\\) \\([^ ]*\\):\\([0-9]+\\)$" 2 3 nil nil 1))
+
+    ;; Errors when running PHPUnit on a test case
+    (add-to-list 'php-regexp-alist
+		 '("^\\(\\/.*\.php\\):\\([0-9]+\\)$" 1 2 nil nil nil))
+    (add-to-list 'php-regexp-alist
+		 '("\\(^.*[Ee]rror.*\\|^.*Exception.*\\) in \\(.*\\) on line \\([0-9]+\\)$" 2 3 nil nil 1))
+    (add-to-list 'php-regexp-alist
+		 '("[0-9]+\. \\([^ ]*\\) \\([^ ]*\\):\\([0-9]+\\)$" 2 3 nil nil 1))))
+
+(defun get-file-in-hierarchy (file)
+  "Searches the directory tree upwards for the given file and returns the path."
+  (setq currentdir (file-name-directory buffer-file-name))
+  (while (and (not (file-exists-p (concat currentdir file)))
+	      (not (string= currentdir "/")))
+    (setq currentdir (file-name-directory (directory-file-name currentdir))))
+  (concat currentdir file))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;;  F3 - Evaluating, programming
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -250,6 +359,9 @@
 ;; Set Zend code style
 (define-key php-mode-map (kbd "<f3> Z") 'php-setenv-zend)
 
+;; Count lines of code
+(define-key php-mode-map (kbd "<f3> <f6>") 'php-loc)
+
 ;; Run php script
 (define-key php-mode-map (kbd "<f3> <f7>") 'php-start-php-on-buffer)
 
@@ -257,13 +369,19 @@
 (define-key php-mode-map (kbd "<f3> <f8>") 'php-start-phpunit-on-buffer)
 
 ;; Check code
-(define-key php-mode-map (kbd "<f3> <f10>") 'php-lint)
+(define-key php-mode-map (kbd "<f3> <f9>") 'php-lint)
 
 ;; Check code style
-(define-key php-mode-map (kbd "<f3> <f11>") 'phpcs-pear)
+(define-key php-mode-map (kbd "<f3> <f10>") 'phpcs)
 
-;; Check Zend code style
-(define-key php-mode-map (kbd "<f3> <f12>") 'phpcs-zend)
+;; Check copy/paste
+(define-key php-mode-map (kbd "<f3> <f11>") 'php-cpd)
+
+;; Run pmd
+(define-key php-mode-map (kbd "<f3> <f12>") 'php-pmd)
+
+;; Run all checks combined
+(define-key php-mode-map (kbd "<f3> <f3>") 'php-compile)
 
 ;; Move between functions
 (define-key php-mode-map (kbd "<f3> <up>")   'beginning-of-defun)

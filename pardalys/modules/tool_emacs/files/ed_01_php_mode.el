4e26e9715d0f5e3f2f1824ce5606017f28b02dbb
Gunnar Wrobel <p@rdus.de>
2009-10-02 11:17:57 +0200

    Allow testing complete packages.

--- pardalys/modules/tool_emacs/files/ed_01_php_mode.el
+++ pardalys/modules/tool_emacs/files/ed_01_php_mode.el
@@ -93,36 +93,36 @@
 
 (setq whitespace-modes (append '(php-mode) whitespace-modes))
 
-(defun php-compile ()
+(defun php-compile (arg)
   "Runs a compilation process with a number of PHP style checks."
-  (interactive)
+  (interactive "p")
   (let ((compilation-error-regexp-alist (get-php-compilation-regexp)))
     (setq cb (current-buffer))
-    (php-loc)
+    (php-loc arg)
     (switch-to-buffer "*compilation*")
     (if (get-buffer  "*php lines of code*")
 	(kill-buffer "*php lines of code*"))
     (rename-buffer "*php lines of code*")
     (switch-to-buffer cb)
-    (php-pmd)
+    (php-pmd arg)
     (switch-to-buffer "*compilation*")
     (if (get-buffer  "*php mess detection*")
 	(kill-buffer "*php mess detection*"))
     (rename-buffer "*php mess detection*")
     (switch-to-buffer cb)
-    (php-cpd)
+    (php-cpd arg)
     (switch-to-buffer "*compilation*")
     (if (get-buffer  "*php copy paste detection*")
 	(kill-buffer "*php copy paste detection*"))
     (rename-buffer "*php copy paste detection*")
     (switch-to-buffer cb)
-    (phpcs)
+    (phpcs arg)
     (switch-to-buffer "*compilation*")
     (if (get-buffer  "*php code style*")
 	(kill-buffer "*php code style*"))
     (rename-buffer "*php code style*")
     (switch-to-buffer cb)
-    (php-lint)
+    (php-lint arg)
     (switch-to-buffer "*compilation*")
     (if (get-buffer  "*php lint*")
 	(kill-buffer "*php lint*"))
@@ -131,100 +131,78 @@
     ))
 
 ;; taken from http://atomized.org/2008/10/php-lint-and-style-checking-from-emacs/
-(defun php-lint ()
+(defun php-lint (arg)
   "Performs a PHP lint check on the current file."
-  (interactive)
-  (let ((compilation-error-regexp-alist (get-php-compilation-regexp)))
-    (compile (concat "php -l -f \"" (buffer-file-name) "\""))))
+  (interactive "p")
+  (setq phpsettings (get-file-in-hierarchy ".emacs.php.el"))
+  (load phpsettings)
+  (let ((compilation-error-regexp-alist (get-php-compilation-regexp))
+	(file-arg (if (> arg 1) (get-one-below-dir-in-hierarchy package_dir)
+		    (buffer-file-name))))
+    (compile (format "%s \"%s\""
+		     phplint_cmd
+		     file-arg))))
 
-(defun phpcs ()
+(defun phpcs (arg)
   "Performs a PHP code sniffer check on the current file."
-  (interactive)
+  (interactive "p")
   (setq phpsettings (get-file-in-hierarchy ".emacs.php.el"))
   (load phpsettings)
-  (let ((compilation-error-regexp-alist (get-php-compilation-regexp)))
-    (compile (concat phpcs_command
+  (let ((compilation-error-regexp-alist (get-php-compilation-regexp))
+	(file-arg (if (> arg 1) (get-one-below-dir-in-hierarchy package_dir)
+		    (buffer-file-name))))
+    (compile (format "%s %s %s"
+		     phpcs_command
 		     phpcs_options
-		     " "
-                     (buffer-file-name)))))
+                     file-arg))))
  
-(defun php-loc ()
-  (interactive)
+(defun php-loc (arg)
+  (interactive "p")
   (if (not buffer-file-read-only)
       (save-buffer))
   (setq phpsettings (get-file-in-hierarchy ".emacs.php.el"))
   (load phpsettings)
-  (let ((compilation-error-regexp-alist (get-php-compilation-regexp)))
-    (set (make-local-variable 'phploc-command)
-	 (format "%s %s %s"
-		 plc_command
-		 plc_options
-		 buffer-file-name))
-    (compile phploc-command)))
+  (let ((compilation-error-regexp-alist (get-php-compilation-regexp))
+	(file-arg (if (> arg 1) (get-one-below-dir-in-hierarchy package_dir)
+		    (buffer-file-name))))
+    (compile (format "%s %s %s"
+		     plc_command
+		     plc_options
+		     file-arg))))
 
-(defun php-cpd ()
-  (interactive)
+(defun php-cpd (arg)
+  (interactive "p")
   (if (not buffer-file-read-only)
       (save-buffer))
   (setq phpsettings (get-file-in-hierarchy ".emacs.php.el"))
   (load phpsettings)
-  (let ((compilation-error-regexp-alist (get-php-compilation-regexp)))
-    (set (make-local-variable 'phpcpd-command)
-	 (format "%s %s %s"
-		 pcp_command
-		 pcp_options
-		 buffer-file-name))
-    (compile phpcpd-command)))
+  (let ((compilation-error-regexp-alist (get-php-compilation-regexp))
+	(file-arg (if (> arg 1) (get-one-below-dir-in-hierarchy package_dir)
+		    (buffer-file-name))))
+    (compile (format "%s %s %s"
+		     pcp_command
+		     pcp_options
+		     file-arg))))
 
-(defun php-pmd ()
-  (interactive)
+(defun php-pmd (arg)
+  (interactive "p")
   (if (not buffer-file-read-only)
       (save-buffer))
   (setq phpsettings (get-file-in-hierarchy ".emacs.php.el"))
   (load phpsettings)
-  (let ((compilation-error-regexp-alist (get-php-compilation-regexp)))
-    (set (make-local-variable 'phpmd-command)
-	 (format "%s;cd %s;%s %s %s %s %s"
-		 pmd_pre
-		 (file-name-directory buffer-file-name)
-		 pmd_command
-		 buffer-file-name
-		 pmd_format
-		 pmd_codestyle
-		 pmd_options))
-    (compile phpmd-command)))
+  (let ((compilation-error-regexp-alist (get-php-compilation-regexp))
+	(file-arg (if (> arg 1) (get-one-below-dir-in-hierarchy package_dir)
+		    (buffer-file-name))))
+  (compile (format "%s;cd %s;%s %s %s %s %s"
+		     pmd_pre
+		     (file-name-directory buffer-file-name)
+		     pmd_command
+		     file-arg
+		     pmd_format
+		     pmd_codestyle
+		     pmd_options))))
 
-(defun php-start-php-on-buffer()
-  (interactive)
-  (if (not buffer-file-read-only)
-      (save-buffer))
-  (setq currentdir (concat (file-name-directory buffer-file-name) "/"))
-  (while (and (not (file-exists-p (concat currentdir ".emacs.php.el")))
-	      (not (string= currentdir "/")))
-    (setq currentdir (file-name-directory (directory-file-name currentdir))))
-  (setq phpsettings (concat currentdir ".emacs.php.el"))
-  (if (file-exists-p phpsettings)
-      (load phpsettings)
-    (setq phpoptions ":/usr/share/php5:/usr/share/php\" -d log_errors=1 -d error_log=\"php-errors.log\" -d error_reporting=\"E_ALL\" "))
-  (let ((compilation-error-regexp-alist ()))
-    (add-to-list 'compilation-error-regexp-alist
-		 '("^\\(\\/.*\.php\\):\\([0-9]+\\)$" 1 2 nil nil nil))
-    (add-to-list 'compilation-error-regexp-alist
-		 '("\\(^.*[Ee]rror.*\\|^.*Exception.*\\) in \\(.*\\) on line \\([0-9]+\\)$" 2 3 nil nil 1))
-    (add-to-list 'compilation-error-regexp-alist
-		 '("[0-9]+\. \\([^ ]*\\) \\([^ ]*\\):\\([0-9]+\\)$" 2 3 nil nil 1))
-    (set (make-local-variable 'phpunit-command)
-	 (concat
-	  "export XDEBUG_CONFIG=\"idekey=php_run\";cd "
-	  (file-name-directory buffer-file-name)
-	  "; php -d include_path=\".:"
-	  (file-name-directory buffer-file-name)
-	  phpoptions
-	  " -f "
-	  (file-name-nondirectory buffer-file-name)))
-    (compile phpunit-command)))
-
-(defun php-start-phpunit-on-buffer (format)
+(defun php-unit (format)
   (interactive (list (completing-read "Output format: "
                                       '(("Default" 1) ("Story" 2) ("Dox" 3))
                                       nil t phpunit-format-history "Default")))
@@ -237,36 +215,15 @@
      nil nil)
     (setq classname (buffer-substring-no-properties
 		     (match-beginning 1) (match-end 1))))
-  (setq currentdir (file-name-directory buffer-file-name))
-  (while (and (not (file-exists-p (concat currentdir ".emacs.php.el")))
-	      (not (string= currentdir "/")))
-    (setq currentdir (file-name-directory (directory-file-name currentdir))))
-  (setq phpsettings (concat currentdir ".emacs.php.el"))
-  (if (file-exists-p phpsettings)
-      (load phpsettings)
-    (progn (setq phpunit_pre "export XDEBUG_CONFIG=\"idekey=php_unit_run\"")
-           (setq phpunit_options "--verbose")
-           (setq phpunit_phpoptions "-d log_errors=1 -d error_log=\"php-errors.log\" -d error_reporting=\"E_ALL\"")
-           (if (compare-strings "/kolab" 0 7
-                                (file-name-directory buffer-file-name) 0 7)
-               ;; A hack to support the default setup for the Kolab environment
-               (progn (setq phpunit_command "/kolab/bin/phpunit")
-                      (setq phpunit_includes "/kolab/lib/php"))
-             (progn (setq phpunit_command "phpunit")
-                    (setq phpunit_includes "/usr/share/php5:/usr/share/php")))))
+  (setq phpsettings (get-file-in-hierarchy ".emacs.php.el"))
+  (load phpsettings)
   (setq phpunit_options (concat phpunit_options
                                 " "
                                 (cond ((string= format "Story") "--story")
                                       ((string= format "Dox") "--testdox")
                                       (t ""))))
   (if classname
-      (let ((compilation-error-regexp-alist ()))
-	(add-to-list 'compilation-error-regexp-alist
-		     '("^\\(\\/.*\.php\\):\\([0-9]+\\)$" 1 2 nil nil nil))
-	(add-to-list 'compilation-error-regexp-alist
-		     '("\\(^.*[Ee]rror.*\\|^.*Exception.*\\) in \\(.*\\) on line \\([0-9]+\\)$" 2 3 nil nil 1))
-	(add-to-list 'compilation-error-regexp-alist
-		     '("[0-9]+\. \\([^ ]*\\) \\([^ ]*\\):\\([0-9]+\\)$" 2 3 nil nil 1))
+      (let ((compilation-error-regexp-alist (get-php-compilation-regexp)))
 	(set (make-local-variable 'phpunit-command)
 	     (format "%s;cd %s;%s %s -d include_path=\".:%s:%s\" %s %s %s"
 	      phpunit_pre
@@ -280,6 +237,25 @@
 	      (file-name-nondirectory buffer-file-name)))
 	(compile phpunit-command))
     (message "No possible test class found!")))
+
+(defun php-start-php-on-buffer()
+  (interactive)
+  (if (not buffer-file-read-only)
+      (save-buffer))
+  (setq phpsettings (get-file-in-hierarchy ".emacs.php.el"))
+  (load phpsettings)
+  (let ((compilation-error-regexp-alist (get-php-compilation-regexp)))
+    (set (make-local-variable 'phpunit-command)
+	 (concat
+	  "export XDEBUG_CONFIG=\"idekey=php_run\";cd "
+	  (file-name-directory buffer-file-name)
+	  "; php -d include_path=\".:"
+	  (file-name-directory buffer-file-name)
+	  phpoptions
+	  " -f "
+	  (file-name-nondirectory buffer-file-name)))
+    (compile phpunit-command)))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -332,6 +308,18 @@
     (setq currentdir (file-name-directory (directory-file-name currentdir))))
   (concat currentdir file))
 
+(defun get-one-below-dir-in-hierarchy (dir)
+  "Searches the directory tree upwards for the given directory and returns the path."
+  (setq currentdir (file-name-directory buffer-file-name))
+  (while (and (not (string= (file-name-nondirectory
+			     (directory-file-name
+			      (file-name-directory
+			       (directory-file-name currentdir))))
+			    dir))
+	      (not (string= currentdir "/")))
+    (setq currentdir (file-name-directory (directory-file-name currentdir))))
+  currentdir)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;  F3 - Evaluating, programming
@@ -366,7 +354,7 @@
 (define-key php-mode-map (kbd "<f3> <f7>") 'php-start-php-on-buffer)
 
 ;; Run unit tests
-(define-key php-mode-map (kbd "<f3> <f8>") 'php-start-phpunit-on-buffer)
+(define-key php-mode-map (kbd "<f3> <f8>") 'php-unit)
 
 ;; Check code
 (define-key php-mode-map (kbd "<f3> <f9>") 'php-lint)

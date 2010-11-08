30192011bffd2efdecaad36cfd11edccac8150b5
Gunnar Wrobel <p@rdus.de>
2010-11-08 22:21:28 +0100

    Add an ubuntu os module.

---
+++ pardalys/modules/os_ubuntu/manifests/init.pp
@@ -0,0 +1,15 @@
+# Class ubuntu::repositories::multiverse
+#
+#  Provides the multiverse repository.
+#  
+# @author Gunnar Wrobel <p@rdus.de>
+# @version 1.0
+# @package os_ubuntu
+#
+class ubuntu::repositories::multiverse
+{
+  @file { 
+    '/etc/apt/sources.list.d/multiverse':
+    source  => 'puppet:///modules/os_ubuntu/repository_multiverse';
+  }
+}

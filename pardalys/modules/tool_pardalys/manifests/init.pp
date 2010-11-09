1ca104ee8d421c9a0b691ec9bdfecfabc80e67c3
Gunnar Wrobel <p@rdus.de>
2010-11-09 12:47:44 +0100

    Quotes.

--- pardalys/modules/tool_pardalys/manifests/init.pp
+++ pardalys/modules/tool_pardalys/manifests/init.pp
@@ -161,7 +161,7 @@ class tool::pardalys {
       package { git-core: ensure => 'installed' }
       package { libmocha-ruby: ensure => 'installed' }
       package { librspec-ruby: ensure => 'installed' }
-      package { libldap-ruby1.8: ensure => 'installed' }
+      package { 'libldap-ruby1.8': ensure => 'installed' }
     }
   }
 

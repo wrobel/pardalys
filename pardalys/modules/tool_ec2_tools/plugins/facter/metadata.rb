95f496559741e4548638b523d4b7d9a8b0dfbcd8
Gunnar Wrobel <p@rdus.de>
2010-11-25 20:17:26 +0100

    Ignore errors in case we can't fetch the metadata.

--- pardalys/modules/tool_ec2_tools/plugins/facter/metadata.rb
+++ pardalys/modules/tool_ec2_tools/plugins/facter/metadata.rb
@@ -3,20 +3,23 @@
 require 'open-uri'
 
 def metadata(id='')
-  open("http://169.254.169.254/2008-02-01/meta-data/#{id}").read.
-    split("\n").each do |o|
-    key = "#{id}#{o.gsub(/\=.*$/, '/')}"
-    if key[-1..-1] != '/'
-      Facter.add("ec2_#{key.gsub(/\-|\//, '_')}".to_sym) do
-        setcode do
-          value = open("http://169.254.169.254/2008-02-01/meta-data/#{key}").
-            read.split("\n")
-          value = value.size > 1 ? value : value.first
+  begin
+    open("http://169.254.169.254/2008-02-01/meta-data/#{id}").read.
+      split("\n").each do |o|
+      key = "#{id}#{o.gsub(/\=.*$/, '/')}"
+      if key[-1..-1] != '/'
+        Facter.add("ec2_#{key.gsub(/\-|\//, '_')}".to_sym) do
+          setcode do
+            value = open("http://169.254.169.254/2008-02-01/meta-data/#{key}").
+              read.split("\n")
+            value = value.size > 1 ? value : value.first
+          end
         end
+      else
+        metadata(key)
       end
-    else
-      metadata(key)
     end
+  rescue
   end
 end
 
